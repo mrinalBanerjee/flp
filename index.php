a673b9dd0f15aa7f08231a2d542ec4cae766ebf0
Mrinal banerjee <61612873+mrinalBanerjee@users.noreply.github.com>
2022-01-17 23:14:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <html>
 <head>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
     <script type='text/javascript'>
         function addFriend(){
             // Number of inputs to create
